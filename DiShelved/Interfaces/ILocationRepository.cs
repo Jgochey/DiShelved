9c100f792566500d1ac97203bf0b34e5ebbfbd5d
Josh Gochey <josh@gochey.net>
2025-05-21 22:56:50 -0500

    Location read functional, prep for other CRUD functions

---
+++ DiShelved/Interfaces/ILocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using DiShelved.Models;
+
+namespace DiShelved.Interfaces
+{
+    public interface ILocationRepository
+    {
+        Task<IEnumerable<Location>> GetAllLocationsAsync();
+        Task<Location> GetLocationByIdAsync(int id);
+        Task<Location> CreateLocationAsync(Location Location);
+        Task<Location> UpdateLocationAsync(int id, Location Location);
+        Task<bool> DeleteLocationAsync(int id);
+    }
+}
---
+++ DiShelved/Interfaces/ILocationService.cs
@@ -0,0 +1,14 @@
+﻿using DiShelved.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DiShelved.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<IEnumerable<Location>> GetAllLocationsAsync();
+        Task<Location?> GetLocationByIdAsync(int id);
+        Task<Location> CreateLocationAsync(Location Location);
+        Task<Location> UpdateLocationAsync(int Id, Location Location);
+        Task<bool> DeleteLocationAsync(int id);
+    }
+}
