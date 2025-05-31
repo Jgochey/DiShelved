555837eb4fc3c7b8d945e0d810e02fc1e1e3726c
Josh Gochey <josh@gochey.net>
2025-05-31 13:11:23 -0500

    Added getallusers

--- DiShelved/Interfaces/IUserRepository.cs
+++ DiShelved/Interfaces/IUserRepository.cs
@@ -5,8 +5,10 @@ namespace DiShelved.Interfaces
   public interface IUserRepository
   {
     Task<User> CreateUserAsync(User User);
-        
+
     Task<User?> GetUserByIdAsync(int id);
+
+    Task<IEnumerable<User>> GetAllUsersAsync();
     
   }
 }
